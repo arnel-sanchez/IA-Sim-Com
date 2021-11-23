f2ac0a22a23485c57603891dbf64e95a9d68fa3a
Samuel Efraín Pupo Wong <62154713+SamuelPupo@users.noreply.github.com>
2021-11-23 16:23:32 -0500

    Update main.py

--- Proyecto/main.py
+++ Proyecto/main.py
@@ -1,10 +1,31 @@
-from simulation import start
+from compilation.tokenizer import Tokenizer
+from simulation.simulator import start
 
 
-def main():
+def test_tokenizer():
+    print("\nCOMPILACION:\n")
+    tokenizer = Tokenizer()
+    file, text = "console", "if;point else point22; 22 3.4; point_1 point;\"AAA\"aaa\n123.0 _a.a"
+    print(text)
+    tokens, error = tokenizer.tokenize(file, text)
+    print()
+    if error is not None:
+        print(error)
+    else:
+        for token in tokens:
+            print(token)
+
+
+def test_simulation():
+    print("\n\nSIMULACION:")
     time = 3  # Tiempo que demora la simulacion de una vuelta
     stop = False  # Reajustes en tiempo real
     start(time, stop)
+
+
+def main():
+    test_tokenizer()
+    test_simulation()
 
 
 if __name__ == '__main__':
