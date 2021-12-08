a86ca47698e28e7850495655b36a1b7da9fde49d
Arnel Sanchez Rodríguez <arnelsanchezrodriguez@gmail.com>
2021-12-08 12:40:52 -0500

    Add Tests at Tokenizer and Parser

--- Proyecto/main.py
+++ Proyecto/main.py
@@ -2,20 +2,6 @@ from compilation.tokenizer import Tokenizer
 from simulation.simulator import start
 
 
-def test_tokenizer():
-    print("\nCOMPILACION:\n")
-    tokenizer = Tokenizer()
-    file, text = "console", "if;point else point22; 22 3.4; point_1 point;\"AAA\"aaa\n123.0 _a.a"
-    print(text)
-    tokens, error = tokenizer.tokenize(file, text)
-    print()
-    if error is not None:
-        print(error)
-    else:
-        for token in tokens:
-            print(token)
-
-
 def test_simulation():
     print("\n\nSIMULACION:")
     time = 3  # Tiempo que demora la simulacion de una vuelta
@@ -24,7 +10,6 @@ def test_simulation():
 
 
 def main():
-    test_tokenizer()
     test_simulation()
 
 
--- Proyecto/tester/test_tokenizer.py
+++ Proyecto/tester/test_tokenizer.py
@@ -10,11 +10,59 @@ from compilation.tokens import TokenType, Token
 class Test_tokenizer(unittest.TestCase):
     def test_A(self):
         tokenizer = Tokenizer()
-        file, text = "console","a"
+        file, text = "console","a=1;"
         tokens, error = tokenizer.tokenize(file, text)
-        t1 = str(tokens[0])
-        t2 = str(Token(TokenType.T_ID, 1,1, "a"))
-        self.assertEqual(t1,t2)
+        tokens_ref = []
+        tokens_ref.append(Token(TokenType.T_ID, 1,1, "a"))
+        tokens_ref.append(Token(TokenType.T_ASSIGN, 1,2))
+        tokens_ref.append(Token(TokenType.T_INT, 1,3, 1))
+        tokens_ref.append(Token(TokenType.T_SEMICOLON, 1,4))
+        for i in range(len(tokens)):
+            t1 = str(tokens[i])
+            t2 = str(tokens_ref[i])
+            self.assertEqual(t1,t2)
+    
+    def test_B(self):
+        tokenizer = Tokenizer()
+        file, text = "console", "if;point else point22; 22 3.4; point_1 point;\"AAA\"aaa\n123.0 _a.a"
+        tokens, error = tokenizer.tokenize(file, text)
+        tokens_ref = []
+        tokens_ref.append(Token(TokenType.T_IF, 1,1))
+        tokens_ref.append(Token(TokenType.T_SEMICOLON, 1,3))
+        tokens_ref.append(Token(TokenType.T_ID, 1,4, "point"))
+        tokens_ref.append(Token(TokenType.T_ELSE, 1,10))
+        tokens_ref.append(Token(TokenType.T_ID, 1,15, "point22"))
+        tokens_ref.append(Token(TokenType.T_SEMICOLON, 1,22))
+        tokens_ref.append(Token(TokenType.T_INT, 1,24, "22"))
+        tokens_ref.append(Token(TokenType.T_DOUBLE, 1,27, "3.4"))
+        tokens_ref.append(Token(TokenType.T_SEMICOLON, 1,30))
+        tokens_ref.append(Token(TokenType.T_ID, 1,32, "point_1"))
+        tokens_ref.append(Token(TokenType.T_ID, 1,40, "point"))
+        tokens_ref.append(Token(TokenType.T_SEMICOLON, 1,45))
+        tokens_ref.append(Token(TokenType.T_STRING, 1,46, "AAA"))
+        tokens_ref.append(Token(TokenType.T_ID, 1,51, "aaa"))
+        tokens_ref.append(Token(TokenType.T_DOUBLE, 2,1, "123.0"))
+        tokens_ref.append(Token(TokenType.T_ID, 2,7, "_a"))
+        tokens_ref.append(Token(TokenType.T_DOT, 2,9))
+        tokens_ref.append(Token(TokenType.T_ID, 2,10, "a"))
+        for i in range(len(tokens)):
+            t1 = str(tokens[i])
+            t2 = str(tokens_ref[i])
+            self.assertEqual(t1,t2)
+
+    def test_C(self):
+        tokenizer = Tokenizer()
+        file, text = "console","a=1;"
+        tokens, error = tokenizer.tokenize(file, text)
+        tokens_ref = []
+        tokens_ref.append(Token(TokenType.T_ID, 1,1, "a"))
+        tokens_ref.append(Token(TokenType.T_ASSIGN, 1,2))
+        tokens_ref.append(Token(TokenType.T_INT, 1,3, 1))
+        tokens_ref.append(Token(TokenType.T_SEMICOLON, 1,4))
+        for i in range(len(tokens)):
+            t1 = str(tokens[i])
+            t2 = str(tokens_ref[i])
+            self.assertEqual(t1,t2)
 
 if __name__ == '__main__':
     unittest.main()
