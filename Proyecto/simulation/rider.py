42185d0ca1c900bd4c97c3b12e4864e9c546743e
Arnel Sanchez Rodríguez <arnelsanchezrodriguez@gmail.com>
2022-02-04 13:37:12 -0500

    Delete Experience

--- Proyecto/simulation/rider.py
+++ Proyecto/simulation/rider.py
@@ -5,16 +5,11 @@ class Rider:
         self.name = name
         self.cornering = cornering
         self.step_by_line = step_by_line
-        self.experience_with_the_bike = 0
         self.probability_of_falling_off_the_motorcycle = 0.000001
 
-    def assign_bike(self, bike: Bike, experience, probability_of_falling_off_the_motorcycle):
+    def assign_bike(self, bike: Bike,probability_of_falling_off_the_motorcycle):
         self.bike = bike
-        self.experience_with_the_bike = experience
         self.probability_of_falling_off_the_motorcycle = probability_of_falling_off_the_motorcycle
-
-    def add_experience(self):
-        self.experience_with_the_bike+=1
 
     def print(self):
         print("Piloto: {}".format(self.name))
