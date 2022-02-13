34ea7db40d926aa2095b9c346c6da36d20ee9cb1
Arnel Sanchez Rodríguez <arnelsanchezrodriguez@gmail.com>
2022-02-12 21:39:16 -0500

    Update

--- Proyecto/tester/test2.py
+++ Proyecto/tester/test2.py
@@ -1,22 +1,10 @@
 from os.path import dirname, abspath
 from sys import path
-from unittest import TestCase, main
-
 
 SCRIPT_DIR = dirname(abspath(__file__))
-path.append(dirname(SCRIPT_DIR))
-
-
-class TestParser(TestCase):
-    def test_A(self):
-        self.assertEqual(1, 1)
-
-    def test_B(self):
-        self.assertEqual(1, 1)
-
-    def test_C(self):
-        self.assertEqual(1, 1)
-
+path[0]=dirname(SCRIPT_DIR)
+path[1]=dirname(SCRIPT_DIR)
+from main import main
 
 if __name__ == '__main__':
-    main()
+    main("Prueba2.pys")
