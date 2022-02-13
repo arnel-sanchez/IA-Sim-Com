4e7f0d066bc60a2b254f23853cc9f451ae0500be
Arnel Sanchez Rodríguez <arnelsanchezrodriguez@gmail.com>
2022-02-12 23:32:12 -0500

    Update Simulator

--- Proyecto/simulation/simulator.py
+++ Proyecto/simulation/simulator.py
@@ -5,8 +5,8 @@ class Simulator:
     def start(self, race: Race):
         print("\nInicio de la carrera\n")
         while True:
-            remove_agents = set()
             for section in race.environment.track.sections:
+                remove_agents = set()
                 for i in range(0, len(race.agents)):
                     if i == 0:
                         if not race.agents[i].overcome_an_obstacle(section, race, race.environment.weather, None,
@@ -29,7 +29,6 @@ class Simulator:
                         elif race.agents[i].shot_down == -1:
                             remove_agents.add(race.agents[i - 1])
                 for x in remove_agents:
-                    print(x.rider.name)
                     race.agents.remove(x)
                 old_weather = race.environment.weather
                 race.environment.change_weather_params()
