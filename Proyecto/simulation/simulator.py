2904f1233dce0b32f341466b5c0e72765d7f9d48
Arnel Sanchez Rodríguez <arnelsanchezrodriguez@gmail.com>
2022-02-12 23:56:18 -0500

    Update

--- Proyecto/simulation/simulator.py
+++ Proyecto/simulation/simulator.py
@@ -9,18 +9,18 @@ class Simulator:
                 remove_agents = set()
                 for i in range(0, len(race.agents)):
                     if i == 0:
-                        if not race.agents[i].overcome_an_obstacle(section, race, race.environment.weather, None,
+                        if len(race.agents) > 1 and not race.agents[i].overcome_an_obstacle(section, race, race.environment.weather, None,
                                                                    race.agents[i + 1]):
                             remove_agents.add(race.agents[i])
                         if race.agents[i].shot_down == 1:
                             remove_agents.add(race.agents[i + 1])
-                    elif i == len(race.agents)-1:
+                    elif len(race.agents) > 1 and i == len(race.agents)-1:
                         if not race.agents[i].overcome_an_obstacle(section, race, race.environment.weather,
                                                                    race.agents[i - 1], None):
                             remove_agents.add(race.agents[i])
                         if race.agents[i].shot_down == -1:
                             remove_agents.add(race.agents[i - 1])
-                    else:
+                    elif len(race.agents) > 1:
                         if not race.agents[i].overcome_an_obstacle(section, race, race.environment.weather,
                                                                    race.agents[i - 1], race.agents[i + 1]):
                             remove_agents.add(race.agents[i])
