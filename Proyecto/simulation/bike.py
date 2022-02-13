95a4119bfb8685b4f32b5945436ee06256a44864
Arnel Sanchez Rodríguez <arnelsanchezrodriguez@gmail.com>
2022-02-13 11:39:38 -0500

    Update Parameters

--- Proyecto/simulation/bike.py
+++ Proyecto/simulation/bike.py
@@ -14,16 +14,16 @@ class Tires(Enum):
 
 
 class Bike:
-    def __init__(self, brand, max_speed, weight, node: BikeNode = None, brakes=5, chassis_stiffness=8):
+    def __init__(self, brand, max_speed, weight, node: BikeNode = None, brakes=5, chassis_stiffness=8, probability_of_the_bike_breaking_down = 1, probability_of_exploding_tires = 1, tires = Tires.Slick_Medium):
         self.brand = brand
         self.max_speed = max_speed
         self.weight = weight
-        self.tires = Tires.Slick_Medium
+        self.tires = tires
         self.brakes = brakes
         self.chassis_stiffness = chassis_stiffness
         self.acceleration = 69.444
-        self.probability_of_the_bike_breaking_down = 0.000001
-        self.probability_of_exploding_tires = 0.000001
+        self.probability_of_the_bike_breaking_down = probability_of_the_bike_breaking_down/1000000
+        self.probability_of_exploding_tires = probability_of_exploding_tires/1000000
         self.node = node
 
     def select_configuration(self, environment: Environment):
--- Proyecto/simulation/rider.py
+++ Proyecto/simulation/rider.py
@@ -2,13 +2,14 @@ from simulation.bike import Bike
 
 
 class Rider:
-    def __init__(self, name, cornering, step_by_line):
+    def __init__(self, name, cornering, step_by_line, probability_of_falling_off_the_bike = 1, aggressiveness = 1, bike = None, independence = 1):
         self.name = name
         self.cornering = cornering
         self.step_by_line = step_by_line
-        self.probability_of_falling_off_the_bike = 0.000001
-        self.aggressiveness = 0.001
-        self.bike = None
+        self.probability_of_falling_off_the_bike = probability_of_falling_off_the_bike/1000000
+        self.aggressiveness = aggressiveness/1000
+        self.bike = bike
+        self.independence = independence/1000000
 
     def assign_bike(self, bike: Bike, probability_of_falling_off_the_bike):
         self.bike = bike
